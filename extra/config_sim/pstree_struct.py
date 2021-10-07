3897428a5d41d4cfad52fc26b3768a266a5bced0
nefanov <efanov@phystech.edu>
2021-10-07 16:40:31 +0300

    pstree input in 3 ways

--- extra/config_sim/pstree_struct.py
+++ extra/config_sim/pstree_struct.py
@@ -1,5 +1,8 @@
 import pydot
 import csv
+import io
+from subprocess import PIPE, run
+
 
 def make_pstree(pslist=[]):
     graph = pydot.Dot()
@@ -10,13 +13,23 @@ def make_pstree(pslist=[]):
 
     return graph
 
+
 def render_pstree(graph, fn="1.png"):
     graph.write_png(fn)
 
 
-def load_ps(fn="1.txt"):
-    #p g s pp name
-    with open(fn) as fp:
+def load_ps(i_stream):
+    with i_stream as fp:
         reader = csv.reader(fp, delimiter=" ", quotechar='"')
         next(reader, None)  # skip the headers 
         return [list(filter(('').__ne__, dr)) for dr in [row for row in reader]]
+
+
+def get_current_host_ps():
+    output = run(["ps", "xao", "pid,pgid,sid,ppid,comm"], stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    return load_ps(io.StringIO(output.stdout))
+
+
+def load_ps_from_fs(fn="1.txt"):
+    #p g s pp name
+    return load_ps(open(fn))
