8c3b6247ed2b8271b748ae299ea0a309bd3f3a12
nefanov <efanov@phystech.edu>
2021-10-07 15:14:51 +0300

    Update interpret.py

--- extra/config_sim/interpret.py
+++ extra/config_sim/interpret.py
@@ -1,31 +1,45 @@
 from simulator import *
 
-def pick_syscall(callname):
-    if callname == 'fork':
-        return SysAPI.fork 
-    elif callname == 'exit':
-        return SysAPI.exit
-    elif callname == 'setsid':
-        return SysAPI.setsid
-    elif callname == 'setpgid':
-        return SysAPI.setpgid
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
+def perform_syscall(API, callname, argv=[]):
+    if callname == 'fork':
+        return API.fork() 
+    elif callname == 'exit':
+        return API.exit(argv)
+    elif callname == 'setsid':
+        return API.setsid()
+    elif callname == 'setpgid':
+        return API.setpgid(argv)
     else:
-        print("Wrong call name:", callname)
-        return None
+        print(bcolors.FAIL+"Wrong call name:"+bcolors.ENDC, callname)
+        return -1
 
 
 def interpret(API, cmd_list=[
-    {'pid':1,'action':('fork',[])}, 
+    {'pid':1,'action':('fork',[])},
+    {'pid':2,'action':('exit',[0])}
     ]):
     
-    for cmd in cmd_list:
+    for i, cmd in enumerate(cmd_list):
         if cmd['pid'] != API.context.self_proc:
-            API.context_switch(cmd['pid'])
-            call = pick_syscall(cmd['action'][0])
-            print("debug:", call)
+            API.context_switch([p for p in API.context.processes if p.p is cmd['pid']][-1])
+            ret = perform_syscall(API, cmd['action'][0], cmd['action'][1])
+            
+            if ret is None or ret < 0:
+                print(bcolors.WARNING+"Interpretation warning on:"+bcolors. ENDC,str(i), "command:","PID", cmd['pid'],":", cmd['action'], "retcode ==", ret)
                  
 
 if __name__ == '__main__':
     API=SysAPI()
     interpret(API)
+    
