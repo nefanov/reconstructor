61631899285f0282f9efb0f0f46ffdde721b1329
nefanov <efanov@phystech.edu>
2021-10-12 16:24:39 +0300

    Update experiments_and_scenarios.py

--- extra/config_sim/experiments_and_scenarios.py
+++ extra/config_sim/experiments_and_scenarios.py
@@ -14,7 +14,38 @@ def load_strace(inp):
         pass
 
 
-def subtree_activity()
+def pstree_to_structured_full_repr(pslist=[]):
+    API = SysAPI()
+    #interpret(API)
+    checkpoint_full_tree_reload(API, get_current_host_ps())
+    T = make_pstree(API.context.processes)
+    nodes = T.get_nodes()
+    for n in nodes:
+        #p = n.get_attributes("p")
+        g = n.get_attributes()["g"]
+        s = n.get_attributes()["s"]
+        pp = n.get_attributes()["pp"]
+        lg = [P for P in nodes if P.get_attributes()["g"]==P.get_attributes()["p"]==g]
+        if (len(lg)>0): # process-group-leader is in pstree
+           T.add_edge(pydot.Edge(lg[0], n, label="group")) 
+        elif  len([P for P in nodes if P.get_attributes()["p"]==g])>1: # process-group-leader is in not pstree, but there is a process with such pid
+            lg = ([P for P in nodes if P.get_attributes()["p"]==g])[0]
+            imn = pydot.Node(s=s,p=lg[0],g=g,pp=-1, label=str(s)+" "+str(s)+" "+str(s)+" "+str(lg[0].get_attrubutes()["pp"]))
+            T.add_edge(imn, lg[0], label="g_leader_deset")
+            T.add_edge(lg[0], n, label="g_leader_imn")
+
+        else: # no such processes
+            imn = pydot.Node(s=s,p=g,g=g,pp=-1, label=str(g)+" "+str(g)+" "+str(s)+" "+str(-1))
+            T.add_edge(imn, n, label="g_leader_imn")
+            
+        ls = [P for P in nodes if P.get_attributes()["s"]==P.get_attributes()["p"]==s]
+        if (len(ls)>0): # process-session-leader is in pstree
+            T.add_edge(pydot.Edge(ls[0], n, label="session"))
+        else: # no such processes
+            imn = pydot.Node(s=s,p=s,g=s,pp=-1, label=str(s)+" "+str(s)+" "+str(s)+" "+str(-1))
+            T.add_edge(imn, n, label="s_leader_imn")
+        
+    return T
 
 def random_syscalls(API, steps=10000, sc_list=['fork', 'exit', 'setsid', 'setpgid'], verbose=False):
     for i in range(steps):
@@ -40,8 +71,13 @@ def random_syscalls(API, steps=10000, sc_list=['fork', 'exit', 'setsid', 'setpgi
     API.util_dump_log()
 
 
-if __name__ == '__main__':
-    for i in range(1):
+def rand_sysc_test(loops = 1, steps = 100):
+    for i in range(loops):
         API = SysAPI()
-        random_syscalls(API=API, steps=100)
+        random_syscalls(API=API, steps=steps)
         del API
+
+if __name__ == '__main__':
+    psl = get_current_host_ps()
+    Tstar = pstree_to_structured_full_repr(pslist=psl)
+    render_pstree(Tstar, "1.png")
