111911f140dbd0f16bdf3f99ead87d10f916a91a
nefanov <efanov@phystech.edu>
2021-10-21 16:02:30 +0300

    Update experiments_and_scenarios.py

--- extra/config_sim/experiments_and_scenarios.py
+++ extra/config_sim/experiments_and_scenarios.py
@@ -1,10 +1,11 @@
-import io
+import os
 import numpy as np
 import pprint
 import random
-import itertools
+import math
 from simulator import *
 from processing import *
+import networkx.algorithms.graph_hashing as graph_hashing
 
 # Print iterations progress
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
@@ -102,9 +103,11 @@ def rand_sysc_test(loops = 1, steps = 100):
         del API
 
     
-def isomorphism_check(G1, G2, def_checker=True):
-    if def_checker:
+def isomorphism_check(G1, G2, checker="WL"):
+    if checker == "DEFAULT":
         res = nx.is_isomorphic(G1, G2)
+    elif checker == "WL":
+        res = math.fabs(int(graph_hashing.weisfeiler_lehman_graph_hash(G1),16) - int(graph_hashing.weisfeiler_lehman_graph_hash(G2),16))
     else:
         #add your custom procedure
         res = False
@@ -125,10 +128,20 @@ def update_sync_query(query, old, new):
     return query
 
 
-def permute_ps(API, verbose=True, prefix="0"):
+def permute_ps(API, allow_shifts=False, verbose=True, prefix="0"):
     query, old = API.extra_return_synced_ps(verbose=False)
     new = list(np.random.permutation(old[1:]))
     new = old[:1] + new
+
+    if allow_shifts == True:
+        #print("Before shifting:", new)
+        for i,item in enumerate(new[1:]):
+            shift = random.randrange(0, 2**16-1, 1)
+            if (item + shift)%2**16-1 not in new[1:]:
+                new[i+1] = (item + shift)%2**16-1
+
+        #print("After shifting:", new)
+
     query = update_sync_query(query, old, new)
     API.extra_make_upload_from_synced(query, save_previous=False)
     if verbose:
@@ -138,25 +151,40 @@ def permute_ps(API, verbose=True, prefix="0"):
     return API
     
     
-def make_permutations(API, iters=100, verbose=False):
+def make_permutations(API, iters=100, shift=False, verbose=False, exp_name="plots"):
+    pydot_obj_trees_list = []
     for i in range(iters):
         if verbose:
             #API.util_ps()
             T = make_pstree(API.context.processes)
-            render_pstree(T, (str)(i)+"_.png")
+            pydot_obj_trees_list.append(T)
+            if not os.path.exists(exp_name):
+                os.makedirs(exp_name)
+            render_pstree(T, exp_name+os.sep+ (str)(i)+"_.png")
 
         printProgressBar(i + 1, iters, prefix = 'Progress:', suffix = 'Complete', length = iters)
-        permute_ps(API)
-        
-        #if verbose:
-        #    API.util_ps()
+        permute_ps(API, allow_shifts=shift)
+    return pydot_obj_trees_list
 
 
 if __name__ == '__main__':
-    if len(sys.argv)>=2 and sys.argv[1] == "-perm":
+    if len(sys.argv)>=2 and sys.argv[1].startswith("-isom"):
         API = SysAPI()
         checkpoint_full_tree_reload(API, get_current_host_ps())
-        make_permutations(API,iters=100)
+        if sys.argv[1].endswith("shift_val"):
+            shift = True
+        else:
+            shift = False
+        bank = make_permutations(API,iters=1000, shift=shift, verbose=True)
+        initial = bank[0]
+        results = []
+        for t in bank[1:]:
+            results.append((isomorphism_check(nx.nx_pydot.from_pydot(initial), nx.nx_pydot.from_pydot(t), checker="WL"), initial, t))
+
+        print("\nResults:\n")
+        pprint.pprint(
+        [(i,r[0]) for i,r in enumerate(results)]
+        )
     else:
         psl = get_current_host_ps()
         Tstar = pstree_to_structured_full_repr(pslist=psl)
